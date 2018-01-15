cc73cc03cb3b367fab7ad7422480314f9c84c6f5
Arkango <asaracino@aryma.it>
2018-01-15 11:25:36 -0500

    gr

--- supermercato.c
+++ supermercato.c
@@ -19,8 +19,10 @@
 #include <stdio.h>
 #include <string.h>
 
+//varibili globali
 #define MAXVALORI 100
 
+//definizione tipi
 typedef struct{
   int codice,
   char[50] name,
@@ -30,3 +32,34 @@ typedef struct{
   int n_employers,
   int revenue
 }supermarket;
+
+//prototipi funzioni/procedure
+void getSupermarket(supermarket VETT[],int i);
+
+//programma principale
+int main(){
+
+  int N;
+  supermarket SUPER[MAXVALORI];
+
+  do{
+    printf("Inserisci il numero di supermercati sui quali operare\n");
+    scanf("%d",&N);
+    if(N <= 0  || N > MAXVALORI)
+          printf("\n Il valore immesso non e' corretto", );
+  }while(N <= 0  || N > MAXVALORI);
+
+  printf("Inserisci supermercati\n");
+
+  for (i = 0; i < N; i++) {
+      SUPER[i]=getSupermarket(SUPER,i);
+  }
+
+  return 0;
+}
+
+void getSupermarket(supermarket VETT[],int i){
+  //procedura che riempie l'array dei supermermercati
+  printf("Inserisci supermecato con codice %d \n",i+1);
+  VETT[i].codice = i;
+}
